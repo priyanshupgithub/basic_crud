a67bf364f250f0fe88dfb171e9ae12e6e4a5388c
Priyanshu Patel <priyanshujii0001@gmail.com>
2025-02-13 20:52:42 +0530

    setup the project using vite and tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <h1 className='text-red-800 text-3xl'>this is the vite + tailwind css project.</h1>
+  )
+}
+
+export default App
