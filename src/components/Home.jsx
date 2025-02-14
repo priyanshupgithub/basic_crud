07b268dd20d5fb419db08f8af62246e7e5b78672
Priyanshu Patel <priyanshujii0001@gmail.com>
2025-02-14 14:09:53 +0530

    Modify Home.jsx by deleting repeated code

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,8 @@
-import React, { useState } from "react";
+import React, { act, useState } from "react";
 import { useNavigate } from "react-router-dom";
 const Home = () => {
   const Navigate = useNavigate();
-  const handleCreate = () => {
-    Navigate("/create-user");
-  };
-  const handleRead = () => {
-    Navigate("/read-user");
-  };
-  const handleUpdate = () => {
-    Navigate("/update-user");
-  };
-  const handleDelete = () => {
-    Navigate("/delete-user");
-  };
+
   return (
     <div>
       <div className="h-screen flex flex-col">
@@ -21,30 +10,15 @@ const Home = () => {
           Welcome to the CRUD Application
         </h1>
         <div className="flex flex-1 flex-col justify-center items-center border border-black m-2 p-2 text-3xl w-full">
-          <h1
-            className="border border-black rounded-full bg-gray-400 cursor-pointer m-2 p-3 hover:bg-gray-500"
-            onClick={handleCreate}
-          >
-            Create the User
-          </h1>
-          <h1
-            className="border border-black rounded-full bg-gray-400 cursor-pointer m-2 p-3 hover:bg-gray-500"
-            onClick={handleRead}
-          >
-            Read the User
-          </h1>
-          <h1
-            className="border border-black rounded-full bg-gray-400 cursor-pointer m-2 p-3 hover:bg-gray-500"
-            onClick={handleUpdate}
-          >
-            Update the User
-          </h1>
-          <h1
-            className="border border-black rounded-full bg-gray-400 cursor-pointer m-2 p-3 hover:bg-gray-500"
-            onClick={handleDelete}
-          >
-            Delete the User
-          </h1>
+          {["Create", "Read", "Update", "Delete"].map((action) => (
+            <h1
+              className="border border-black rounded-full bg-gray-400 cursor-pointer m-2 p-3 hover:bg-gray-500"
+              key={action}
+              onClick={() => Navigate(`/${action.toLowerCase()}-user`)}
+            >
+              {`${action} the user`}
+            </h1>
+          ))}
         </div>
       </div>
     </div>
